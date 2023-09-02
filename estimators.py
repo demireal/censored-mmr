e7a2729c0ac935cf32cfff4a0cedc783c1e2e99c
Ilker Demirel <demirel@chronos.csail.mit.edu>
2023-09-02 01:19:06 -0400

    finished implementing DR and did some polishing for readability. tests seem pretty good. seems that we will need to trim a few instances where T gets too big and survival functions explode. ATE estimation from DR signal seemed fine after doing that (i.e. disregardind 1 or 2 exploding instances that correspond to the biggest T values in the whole dataframe, which makes sense)

--- estimators.py
+++ estimators.py
@@ -8,11 +8,6 @@ from lifelines import CoxPHFitter
 from SyntheticDataModule import *
 from scipy.integrate import quad
 from scipy.interpolate import interp1d
-import concurrent.futures
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-import tqdm
-
 from sklearn.metrics.pairwise import rbf_kernel, laplacian_kernel
 
 
@@ -36,15 +31,13 @@ def prop_score_est(df, target, feature, model_name):
     if model_name == 'logistic':
         logit_model = sm.Logit(y, X)
         result = logit_model.fit(disp=0)
-        probs = result.predict(X)
+        return result.predict(X)
 
     elif model_name == 'mean':
-        probs = y.mean() * np.ones(len(y))
+        return y.mean() * np.ones(len(y))
 
     else:
         raise NotImplementedError(f'{model_name} is not implemented for propensity score estimation')
-    
-    return probs
 
 
 def coxph_base_surv(df, cov_list, flip=False):
@@ -53,12 +46,13 @@ def coxph_base_surv(df, cov_list, flip=False):
 
     @params:
         df: Data (pd.DataFrame)
+        cov_list: regressor features (list of strings)
         flip: whether to flip the censoring indicator (bool). E.g., "True" if fitting the model on censoring variable
 
     @returns:
-        t_event: event/censoring times
-        est_base_surv: corresponding (to t_event) baseline survival probabilities
-        cph.params_: estimated hazards (beta)
+        t_event: event/censoring times (np.array)
+        est_base_surv: corresponding baseline survival probabilities (np.array)
+        cph.params_: beta estimates (np.array)
     '''
 
     crop_df = df[cov_list + ['T', 'Delta']].copy()
@@ -82,23 +76,26 @@ def est_surv(df, cov_list, tte_model):
     
     @params:
         df: Data (pd.DataFrame)
-        cov_list: list of covariates except from S and A (string)
+        cov_list: covariates (list of strings) 
         tte_model: generative model for the outcome (censoring variable) to be fit
+        
+    @return:
+        separate dictionaries (for Y and C) that contain the survival model parameters for different S and A variables
     '''
 
-    cbse = {}  # censoring-baseline-survival-estimate (cbse) 
-    ybse = {}  # TimeToEvent-baseline-survival-estimate (ybse, TTE is y)
+    Fb_Y = {}  # dictionary for TimeToEvent (Y) baseline survival estimate  
+    Gb_C = {}  # dictionary for CensoringTime (C) baseline survival estimate 
         
     for s in range(2):
         for a in range(2):
             
             # Estimate the survival function for the censoring variable C
             if len(df.query(f'S=={s} & A=={a} & Delta==0')) == 0:  # deal with "lifelines" lib errors 
-                cbse[f't_S{s}_A{a}'], cbse[f'St_S{s}_A{a}'] = [-1], [1]
+                Gb_C[f't_S{s}_A{a}'], Gb_C[f'St_S{s}_A{a}'], Gb_C[f'beta_S{s}_A{a}'] = [-1], [1], np.zeros(len(cov_list))
                 
             else:
                 if tte_model == 'coxph':
-                    cbse[f't_S{s}_A{a}'], cbse[f'St_S{s}_A{a}'], cbse[f'beta_S{s}_A{a}'] = \
+                    Gb_C[f't_S{s}_A{a}'], Gb_C[f'St_S{s}_A{a}'], Gb_C[f'beta_S{s}_A{a}'] = \
                     coxph_base_surv(df.query(f'S=={s} & A=={a}').copy(), cov_list[1:], flip=True) # fit for C 
                     
                 else:
@@ -106,187 +103,160 @@ def est_surv(df, cov_list, tte_model):
                     
             # Estimate the survival function for the time-to-event variable Y
             if len(df.query(f'S=={s} & A=={a} & Delta==1')) == 0:
-                ybse[f't_S{s}_A{a}'], ybse[f'St_S{s}_A{a}'] = [-1], [1]
+                Fb_Y[f't_S{s}_A{a}'], Fb_Y[f'St_S{s}_A{a}'], Fb_Y[f'beta_S{s}_A{a}'] = [-1], [1], np.zeros(len(cov_list))
                 
             else:
                 if tte_model == 'coxph':
-                    ybse[f't_S{s}_A{a}'], ybse[f'St_S{s}_A{a}'], ybse[f'beta_S{s}_A{a}'] = \
+                    Fb_Y[f't_S{s}_A{a}'], Fb_Y[f'St_S{s}_A{a}'], Fb_Y[f'beta_S{s}_A{a}'] = \
                     coxph_base_surv(df.query(f'S=={s} & A=={a}').copy(), cov_list[1:], flip=False) # fit for Y
                     
                 else:
                     raise NotImplementedError(f'Time-to-event model <{tte_model}> is not implemented.')
                     
-    return cbse, ybse 
+    return Gb_C, Fb_Y 
 
 
-def eval_surv(s, a, x, T, surv_dict):
+def eval_surv_(t, St, T):
     '''
-    Evaluate the value of a survival function specificied by "surv_dict, s, a" at "T" with covariates "x"
+    Evaluate P(Y>T) = St(T)
     '''
     
-    t_arr = surv_dict[f't_S{s}_A{a}']
-    st_arr = surv_dict[f'St_S{s}_A{a}']
-    
-    if list(t_arr) == [-1]:
-        res = 1
-    else:
-        beta_arr = surv_dict[f'beta_S{s}_A{a}']
-        base_val = st_arr[np.argmin(np.abs(T - t_arr))]
-        res = base_val ** (np.exp(beta_arr @ x))
-    
-    return res   
-
-    
-#Conditional survival function (P(Y>yy|Y>cc))
-def cond_surv(yy,cc,ybse, a,s, x, S0_t):
-    """
-    Returns the probability that Y>yy | Y>cc
-    """
-    if yy < cc: # survival probability is one if time is smaller than the condition
+    if list(t) == [-1]:
         return 1
     else:
-        beta_arr = ybse[f'beta_S{s}_A{a}'] 
+        return np.interp(T, t, St)
+    
 
-        return (np.interp(yy, ybse[f't_S{s}_A{a}'], ybse[f'St_S{s}_A{a}']) / S0_t)** (np.exp(beta_arr @ x))
+def eval_cond_surv_(y, c, t, St):
+    '''
+    Evaluate P(Y>y|Y>c) 
+    The implementation here is correct under "(conditional) independence" between Y and C
+    '''
+    
+    if y < c: # survival probability is one if time is smaller than the condition
+        return 1
+    else:
+        num = eval_surv_(t, St, y)
+        denum = eval_surv_(t, St, c)
+        return num / denum 
 
 
-def eval_Qfunc_ratio(s,a,x,T,ybse, thresh = 1e-6):
-    """
+def eval_Qfunc_(s, a, x, T, Fb_Y, thresh=1e-6):
+    '''
     Q function with the ratio method + additional checks for stability
+    '''
+    
+    Fb_sa = Fb_Y[f'St_S{s}_A{a}']               # Fb(t|S=s,A=a) = P(Y>t|S=s,A=a) (baseline survival function for Y)
+    Fb_sa_t = Fb_Y[f't_S{s}_A{a}']              # t indices for Fb(t|S=s,A=a)
+    Fb_sa_beta = Fb_Y[f'beta_S{s}_A{a}']        # CoxPH param. estimates for Fb(t|X,S=s,A=a)
+    Fb_sax = Fb_sa ** (np.exp(Fb_sa_beta @ x))  # Fb(t|X=x,S=s,A=a) = P(Y>t|X=x,S=s,A=a)
+    
+    norm = eval_surv_(Fb_sa_t, Fb_sax, T) # normalization constant F(T|X,S,A)
+    t_max = Fb_sa_t.max()
 
-    """
-    S0_t = np.interp(T, ybse[f't_S{s}_A{a}'], ybse[f'St_S{s}_A{a}'])
-
-    beta_arr = ybse[f'beta_S{s}_A{a}'] 
-    if (S0_t** (np.exp(beta_arr @ x))) < thresh: # If survival probability is too small, we just return the conditionned time.
-        return T
-
-    FY_C_S_A = lambda yy, cc : cond_surv(yy,cc, ybse,a =a ,s = s, x = x, S0_t = S0_t )
-    t_max = np.max(np.max(ybse[f't_S{s}_A{a}']))
-    if T>=t_max:
-        return T # When we are outside the support, we assume instantaneous failure.
+    if (norm < thresh) or (T >= t_max): # return T itself when it is too big to explode the denum. or outside support
+        return T 
     else:
-        return quad(lambda y : FY_C_S_A(y,T) ,a = 0, b=t_max, points = 100 )[0] 
-
-def eval_Qfunc(s, a, x, T, ybse):  
+        Fb_csax = lambda yy, cc : eval_cond_surv_(yy, cc, Fb_sa_t, Fb_sax)  # Fb(t|X=x,Y>cc,S=s,A=a) = P(Y>t|X=x,Y>cc,S=s,A=a)
+        return quad(lambda y : Fb_csax(y,T), a=0, b=t_max, limit=10)[0]
     
-    t_arr = ybse[f't_S{s}_A{a}']
-    st_arr = ybse[f'St_S{s}_A{a}']
-    beta_arr = ybse[f'beta_S{s}_A{a}'] 
     
-    denum = eval_surv(s, a, x, T, ybse)
+def eval_Qfunc_arr_(s, a, x, Gb_sa_t_idx, Fb_Y):  
+    '''
+    Evaluate the Q function for all the "C" values in array Gb_sa_t_idx
+    '''
     
-    stx_arr = st_arr ** (np.exp(beta_arr @ x))
-    func = interp1d(t_arr, stx_arr, kind='linear', fill_value='extrapolate')
-    num, _ = quad(func, T, t_arr.max(), limit=10)
+    Fb_sa = Fb_Y[f'St_S{s}_A{a}']               # Fb(t|S=s,A=a) = P(Y>t|S=s,A=a) (baseline survival function for Y)
+    Fb_sa_t = Fb_Y[f't_S{s}_A{a}']              # t indices for Fb(t|S=s,A=a)
+    Fb_sa_beta = Fb_Y[f'beta_S{s}_A{a}']        # CoxPH param. estimates for Fb(t|X,S=s,A=a)
+    Fb_sax = Fb_sa ** (np.exp(Fb_sa_beta @ x))  # Fb(t|X=x,S=s,A=a) = P(Y>t|X=x,S=s,A=a)
     
-    res = num / denum 
+    Fb_denum = np.array(list(map(lambda c: eval_surv_(Fb_sa_t, Fb_sax, c), Gb_sa_t_idx)))
     
-    return res, num, denum 
-
-
-def eval_Qfunc_onarray(s, a, x, gt_arr, ybse):  
+    t_max = Fb_sa_t.max() # a proxy for the infinity upper bound on the integral
+    t_int = np.append(Gb_sa_t_idx, t_max)
     
-    denum = np.array([eval_surv(s, a, x, T, ybse) for T in gt_arr])
+    func = interp1d(Fb_sa_t, Fb_sax, kind='linear', fill_value='extrapolate')  # Fb(t|X=x,S=s,A=a)
+    # Calculate individual areas of Integral(Fb(t|..)) between every value in Gb_sa_t_idx and finally to infinity (tmax)
+    interval_integrals = np.array(list(map(lambda c1, c2: quad(func, a=c1, b=c2, limit=2)[0], t_int[:-1], t_int[1:])))
     
-    t_arr = ybse[f't_S{s}_A{a}']
-    st_arr = ybse[f'St_S{s}_A{a}']
-    beta_arr = ybse[f'beta_S{s}_A{a}'] 
-    stx_arr = st_arr ** (np.exp(beta_arr @ x))
-    func = interp1d(t_arr, stx_arr, kind='linear', fill_value='extrapolate')
-    
-    func_vals = np.array(list(map(func, gt_arr)))
-    gt_arr = np.append(gt_arr, t_arr.max())
-    gt_diff_arr = [b - a for a, b in zip(gt_arr, gt_arr[1:])]
-    
-    areas = func_vals * gt_diff_arr
-    shift_cumsum = np.roll(np.cumsum(areas), 1)
+    # Three lines below compute "integrals" which is equal to Fb(c|..) for every c in Gb_sa_t_idx
+    shift_cumsum = np.roll(np.cumsum(interval_integrals), 1)
     shift_cumsum[0] = 0  
-    num = np.sum(areas) - shift_cumsum
-
-    res = num / denum 
+    Fb_num = np.sum(interval_integrals) - shift_cumsum
     
-    return res
+    return (Fb_num / Fb_denum) + Gb_sa_t_idx 
 
 
-def eval_int_term(s, a, x, T, cbse, ybse):
+def eval_int_term_(s, a, x, T, Gb_C, Fb_Y):
     
-    t_arr = cbse[f't_S{s}_A{a}']
+    Gb_sa_t = Gb_C[f't_S{s}_A{a}']        # t indices for Gb(t|S=s,A=a)
+    eval_idx = np.where(Gb_sa_t < T)[0]   # indices to evaluate the integral for
+    Gb_sa_t_idx = Gb_sa_t[eval_idx].copy()
     
-    if list(t_arr) == [-1]:
-        res = 0    
+    if (list(Gb_sa_t) == [-1]) or (len(eval_idx) == 0): 
+        return 0    
+
     else:
-        if len(np.where(t_arr < T)[0]) == 0:
-            res = 0
-        else:
-            st_arr = cbse[f'St_S{s}_A{a}']
-            beta_arr = cbse[f'beta_S{s}_A{a}']
-            new_st_arr, idx = np.unique(st_arr, return_index=True)
-            new_st_arr, idx = new_st_arr[::-1], idx[::-1]
-            t_arr = t_arr[idx]
-            
-            stx_arr = new_st_arr ** (np.exp(beta_arr @ x))
-            Gc = 1 - stx_arr
-            Gc = np.append(Gc, 0)
-            dGc = [b - a for a, b in zip(Gc, Gc[1:])]
-            start_time = time()
-            num = eval_Qfunc_onarray(s, a, x, t_arr, ybse)
-            #print('Time elapsed for Q-function integration: {:.2f}'.format(time() - start_time))
-            denum = np.array([eval_surv(s, a, x, c, cbse) ** 2 for c in t_arr])
-            res = np.sum(dGc * num / denum)            
-            
-#             ub_ind = np.where(t_arr < T)[0][-1] + 1 # get upper bound index for T
-
-#             t_arr = cbse[f't_S{s}_A{a}'][:ub_ind]
-#             st_arr = cbse[f'St_S{s}_A{a}'][:ub_ind]
-#             beta_arr = cbse[f'beta_S{s}_A{a}'] 
-
-#             stx_arr = st_arr ** (np.exp(beta_arr @ x))
-                
-#             if len(stx_arr) == 1:
-#                 res = (1 - stx_arr[0]) * eval_Qfunc(s, a, x, t_arr[0], ybse) / (eval_surv(s, a, x, t_arr[0], cbse) ** 2)
-#             else:            
-#                 deriv = np.gradient(1 - stx_arr)
-#                 start_time = time()
-#                 num = eval_Qfunc_onarray(s, a, x, t_arr, ybse)
-#                 #print('Time elapsed for Q-function integration: {:.2f}'.format(time() - start_time))
-#                 #num = np.array([eval_Qfunc(s, a, x, c, ybse) for c in t_arr])
-#                 denum = np.array([eval_surv(s, a, x, c, cbse) ** 2 for c in t_arr])
-#                 res = np.sum(deriv * num / denum)     
+        Gb_sa = Gb_C[f'St_S{s}_A{a}']               # Gb(t|S=s,A=a) = P(C>t|S=s,A=a) (baseline survival function for C)
+        Gb_sa_beta = Gb_C[f'beta_S{s}_A{a}']        # CoxPH param. estimates for Gb(t|X,S=s,A=a)
+        Gb_sax = Gb_sa ** (np.exp(Gb_sa_beta @ x))  # Gb(t|X=x,S=s,A=a) = P(C>t|X=x,S=s,A=a)
+        
+        Gb_sax_idx = Gb_sax[eval_idx].copy()
+     
+        start_time = time()
     
-    return res
-
-
-def eval_Ystar(s, a, x, delta, T, cbse, ybse):
+        G_sax_idx = 1 - Gb_sax_idx
+        dG_sax_idx = [b - a for a, b in zip(np.insert(G_sax_idx, 0, 0), np.insert(G_sax_idx, 0, 0)[1:])]    
+        Q_num = eval_Qfunc_arr_(s, a, x, Gb_sa_t_idx, Fb_Y)
+        Gbsq_denum = np.array(list(map(lambda c: eval_surv_(Gb_sa_t, Gb_sax, c) ** 2, Gb_sa_t_idx)))
+        
+        print('Time elapsed for compting the integral term: {:.2f}'.format(time() - start_time))
+        
+        return np.sum(dG_sax_idx * Q_num / Gbsq_denum)            
     
-    if delta == 1:
-        ft_num = T
+
+def eval_Ystar_(s, a, x, Delta, T, Gb_C, Fb_Y):
+    '''
+    Evaluate Y*_SA(X,Y,C)
+    Note that T = min(Y,C), and it is enough for the calculation of this signal.
+    '''
+    
+    if Delta == 1: # decide the numerator of the first term (ft) based on the value of Delta
+        ft_num = T  
     else:
-        ft_num = eval_Qfunc(s, a, x, T, ybse)
+        ft_num = eval_Qfunc_(s, a, x, T, Fb_Y)
 
-    first_term = ft_num / eval_surv(s, a, x, T, cbse) 
-    res = first_term - eval_int_term(s, a, x, T, cbse, ybse)
+    Gb_sa = Gb_C[f'St_S{s}_A{a}']               # Gb(t|S=s,A=a) = P(C>t|S=s,A=a) (baseline survival function for C)
+    Gb_sa_t = Gb_C[f't_S{s}_A{a}']              # t indices for Gb(t|S=s,A=a)
+    Gb_sa_beta = Gb_C[f'beta_S{s}_A{a}']        # CoxPH param. estimates for Gb(t|X,S=s,A=a)
+    Gb_sax = Gb_sa ** (np.exp(Gb_sa_beta @ x))  # Gb(t|X=x,S=s,A=a) = P(C>t|X=x,S=s,A=a)  
     
-    return res
-
-
-def eval_mu(s, a, x, ybse):
-    t_arr = ybse[f't_S{s}_A{a}']
-    st_arr = ybse[f'St_S{s}_A{a}']
-    beta_arr = ybse[f'beta_S{s}_A{a}'] 
+    ft_denum = eval_surv_(Gb_sa_t, Gb_sax, T)   # first term (ft) denumerator, always uses "T" regardless of the numerator
     
-    stx_arr = st_arr ** (np.exp(beta_arr @ x))
-    func = interp1d(t_arr, stx_arr, kind='nearest-up', fill_value='extrapolate')
-    res, _ = quad(func, 0, t_arr.max(), limit=4)
-    
-    return res     
+    ft = ft_num / ft_denum # calculate first term (ft)
+    return ft - eval_int_term_(s, a, x, T, Gb_C, Fb_Y)
 
 
-def fill_barG(df, cov_list, cbse):
+def eval_mu_(s, a, x, Fb_Y):
     '''
-    Fill the values for G_bar(T|X,S,A) that are used in both the IPCW and the DR signal.
+    Evaluate \mu_SA(X)
     '''
-    df['G_bar(T|X,S,A)'] = df.apply(lambda r: eval_surv(int(r['S']), int(r['A']), r[cov_list], r['T'], cbse), axis=1)
+    Fb_sa = Fb_Y[f'St_S{s}_A{a}']               # Fb(t|S=s,A=a) = P(Y>t|S=s,A=a) (baseline survival function for Y)
+    Fb_sa_t = Fb_Y[f't_S{s}_A{a}']              # t indices for Fb(t|S=s,A=a)
+    Fb_sa_beta = Fb_Y[f'beta_S{s}_A{a}']        # CoxPH param. estimates for Fb(t|X,S=s,A=a) 
+    Fb_sax = Fb_sa ** (np.exp(Fb_sa_beta @ x))  # Fb(t|X=x,S=s,A=a) = P(Y>t|X=x,S=s,A=a)
+        
+    func = interp1d(Fb_sa_t, Fb_sax, kind='nearest-up', fill_value='extrapolate')
+    return quad(func, a=0, b=Fb_sa_t.max(), limit=10)[0]    
+
+
+def fill_barG(df, cov_list, Gb_C):
+    '''
+    Fill the values for Gb(T|X,S,A) = P(C>T|X,S,A)   that are used in both the IPCW and the DR signal.
+    '''
+    df['Gb(T|X,S,A)'] = df.apply(lambda r:\
+     eval_surv_(Gb_C[f"t_S{int(r['S'])}_A{int(r['A'])}"], Gb_C[f"St_S{int(r['S'])}_A{int(r['A'])}"], r['T']), axis=1)
     
 
 def ipcw_est(df, S):
@@ -307,7 +277,7 @@ def ipcw_est(df, S):
             part0 = (1 - row['A']) / (1 - row['P(A=1|X,S)'])
 
             psx = row['S'] * row['P(S=1|X)'] + (1 - row['S']) * (1 - row['P(S=1|X)'])
-            denom = psx * row['G_bar(T|X,S,A)']
+            denom = psx * row['Gb(T|X,S,A)']
 
             ipcw = row['T'] * (part1 - part0) / denom
 
@@ -330,14 +300,14 @@ def cdr_est(df, cov_list, cbse, ybse, S):
             
 #             t1 = time()
             
-            mu_xsa1 = eval_mu(S, 1, row[cov_list], ybse)
-            mu_xsa0 = eval_mu(S, 0, row[cov_list], ybse)
+            mu_xsa1 = eval_mu_(S, 1, row[cov_list], ybse)
+            mu_xsa0 = eval_mu_(S, 0, row[cov_list], ybse)
             
 #             t2 = time()
 #             print(f"mu time: {t2 - t1:.4f}")
             
             mu_xs = mu_xsa1 - mu_xsa0
-            Ystar_xsa = eval_Ystar(S, aind, row[cov_list], row['Delta'], row['T'], cbse, ybse)
+            Ystar_xsa = eval_Ystar_(S, aind, row[cov_list], row['Delta'], row['T'], cbse, ybse)
             
 #             t3 = time()
 #             print(f"YSTAR time: {t3 - t2:.4f}")
@@ -423,8 +393,8 @@ def single_mmr_run(test_signals, save_df, d, rct_size, os_size, B, kernel, cov_l
     df_combined.loc[df_combined.S==0, 'P(A=1|X,S)'] = prop_score_est(df_combined.query('S==0').copy(), 'A', cov_list, 'logistic')
     df_combined.loc[df_combined.S==1, 'P(A=1|X,S)'] = prop_score_est(df_combined.query('S==1').copy(), 'A', cov_list, 'logistic')
 
-    cbse, ybse = est_surv(df_combined, cov_list, tte_model='coxph')
-    fill_barG(df_combined, cov_list, cbse)
+    Gb_C, Fb_Y = est_surv(df_combined, cov_list, tte_model='coxph')
+    fill_barG(df_combined, cov_list, Gb_C)
 
     if any("IPCW" in key for key in test_signals.keys()):
         ipcw_est(df_combined, S=0)
@@ -435,8 +405,8 @@ def single_mmr_run(test_signals, save_df, d, rct_size, os_size, B, kernel, cov_l
         ipw_est(df_combined, S=1, baseline='impute')  # censored observations are IMPUTED
         
     if any("CDR" in key for key in test_signals.keys()):
-        cdr_est(df_combined, cov_list, cbse, ybse, S=0)  
-        cdr_est(df_combined, cov_list, cbse, ybse, S=1)  
+        cdr_est(df_combined, cov_list, Gb_C, Fb_Y, S=0)  
+        cdr_est(df_combined, cov_list, Gb_C, Fb_Y, S=1)  
     
     
     # Estimate the nuisance parameters for the combined dataframe with censored observations dropped
