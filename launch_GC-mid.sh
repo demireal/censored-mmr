9083b1aa36b8549b5af082697119872d1560b9c6
Ilker Demirel <demirel@chronos.csail.mit.edu>
2023-09-17 19:07:48 -0400

    fixed UC=1 in launch-mid.sh

--- launch_GC-mid.sh
+++ launch_GC-mid.sh
@@ -33,29 +33,29 @@
 #python main.py --json_path=ihdp/diffPOihdp-highdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPCW_GC
 
 python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPCW
-python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
 python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Impute
 python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Drop
 python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Impute
 python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/diffPOihdp-midlowdiff_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
 
 python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPCW
-python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
 python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Impute
 python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Drop
 python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Impute
 python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/diffPOihdp-midhighdiff_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
 
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPCW
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Impute
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Drop
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Impute
-python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPCW
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPW-Impute
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPW-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=DR-Impute
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midlowhc_GC.json --UC=1 --CD=10 --M=3 --signals=CDR
 
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPCW
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=CDR
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Impute
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=IPW-Drop
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Impute
-python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=0 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPCW
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPW-Impute
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=IPW-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=DR-Impute
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=DR-Drop
+python3 main.py --json_path=ihdp/samePOihdp-midhighhc_GC.json --UC=1 --CD=10 --M=3 --signals=CDR
